7c4ae86e67026e3db09c97ee7117b6d8b6c71dac
Alan <devaliant94@gmail.com>
2020-09-26 19:51:55 -0300

    components with ng generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Saludo } from './saludocomponente/saludo.component'
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    Saludo
+    HolaMundoComponent
   ],
   imports: [
     BrowserModule
